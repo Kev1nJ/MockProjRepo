7ff1b43742e5f762e3805af981f80bb18eaab0f9
Kev1nJ <kevinjerome85@yahoo.com>
2023-10-17 14:52:16 -0400

    commiting

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project2
+
+This is a test commit
+
+This is a commit from Kevin
+
+this is a commit from ezio 
---
+++ models/index.js
@@ -0,0 +1,10 @@
+
+const User = require('./User');
+const Gallery = require('./Ingredient');
+const Painting = require('./Recipe');
+
+//Define associate between Ingredient, User, and Recipe Models
+
+
+
+module.exports = { User, Recipe, Ingredient };
